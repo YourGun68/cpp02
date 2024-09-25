f983935ecb70c5de1448122e826a78e53ec82606
Peter <herisandre68@gmail.com>
2024-09-25 14:57:17 +0200

    update

---
+++ ex00/fixed.hpp
@@ -0,0 +1,25 @@
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+private:
+	/* data */
+public:
+	Fixed(/* args */);
+	~Fixed();
+};
+
+Fixed::Fixed(/* args */)
+{
+}
+
+Fixed::~Fixed()
+{
+}
+
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpeter <jpeter@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/18 14:01:48 by jpeter            #+#    #+#             */
+/*   Updated: 2024/08/18 11:42:45 by jpeter           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fixed.hpp"
+
+int	main( void )
+{
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+	
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
